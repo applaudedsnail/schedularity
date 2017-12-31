692f0ab413e432b1fe9880454f419e493e95b6a3
Jesus <jsalcedo6@ucmerced.edu>
2017-12-31 14:12:06 -0800

    Finished finetuning algorithm and made the schedules and employees into objects

--- managerSchedule.php
+++ managerSchedule.php
@@ -63,6 +63,8 @@ tr:nth-child(even) {
 
   <?php
 include "employee.inc";
+include "shift.inc";
+include "employeeSchedule.inc";
 define('DB_NAME', 'id4019241_users');
 define('DB_USER', 'id4019241_jesus');
 define('DB_PASSWORD', 'jesus242');
@@ -72,7 +74,11 @@ $link = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 $sql = "SELECT id, month, day, hour, min, ehour, emin, user, covered FROM shift";
 
+$sql2 = "SELECT id, month, day, hour, min, ehour, emin, user, avail FROM datetime";
+
 $result = $link->query($sql);
+
+$result2 = $link->query($sql2);
 
 $name = $_SESSION['user'];
 
@@ -96,15 +102,11 @@ $min2 = array();
 $ehour2 = array();
 $emin2 = array();
 $user2 = array();
-$tshift = array();
-
-$sql2 = "SELECT avail
-			FROM datetime
-				WHERE month = ? AND day = ?";
-
-$stmt = $link->prepare($sql2);
+$avail = array();
 
 $myEmployee = array();
+$myShift = array();
+$myEmployeeSchedule = array();
 
 //Stores information of shift
 if ($result->num_rows > 0) {
@@ -118,148 +120,121 @@ if ($result->num_rows > 0) {
 		$emin[$i] = $row["emin"];
 		$covered[$i] = $row["covered"];
 
-		$myEmployee[$i] = new employee($user[$i], $day[$i], $month[$i], $hour[$i], $min[$i], $ehour[$i], $emin[$i], $covered[$i]);
+		$myShift[$i] = new shift($user[$i], $day[$i], $month[$i], $hour[$i], $min[$i], $ehour[$i], $emin[$i], $covered[$i]);
 
-		/*
-			echo "This is user: $user[$i] <br>";
-
-			$mTest = $month[$i];
-			$dTest = $day[$i];
-
-			echo "This is mTest: $mTest <br>";
-			echo "This is dTest: $dTest <br>";
-
-			$stmt->bind_param("ii", $mTest, $dTest);
-
-			$stmt->execute();
-			$stmt->bind_result($res);
-
-			$tshift[$i] = $res;
-
-			echo "This is: $tshift[$i] <br>";
-		*/
 		$i++;
 	}
 }
-foreach ($myEmployee as $value) {
-	if ($value->day == 6) {
-		echo "Success <br>";
-	}
-
-}
-var_dump($myEmployee);
-
-/*
 
 //Stores information from employee schedulee
 if ($result2->num_rows > 0) {
-while ($row = $result2->fetch_assoc()) {
-$user2[$i2] = $row["user"];
-$month2[$i2] = $row["month"];
-$day2[$i2] = $row["day"];
-$hour2[$i2] = $row["hour"];
-$min2[$i2] = $row["min"];
-$ehour2[$i2] = $row["ehour"];
-$emin2[$i2] = $row["emin"];
-$i2++;
-}
+	while ($row2 = $result2->fetch_assoc()) {
+		$user2[$i2] = $row2["user"];
+		$month2[$i2] = $row2["month"];
+		$day2[$i2] = $row2["day"];
+		$hour2[$i2] = $row2["hour"];
+		$min2[$i2] = $row2["min"];
+		$ehour2[$i2] = $row2["ehour"];
+		$emin2[$i2] = $row2["emin"];
+		$avail[$i2] = $row2["avail"];
+		$myEmployeeSchedule[$i2] = new employeeSchedule($user2[$i2], $day2[$i2], $month2[$i2], $hour2[$i2], $min2[$i2], $ehour2[$i2], $emin2[$i2], $avail[$i2]);
+
+		$i2++;
+	}
 }
 
 //Stores information from employee
 $i3 = 0;
 $userHour = array();
 $userEmployee = array();
-$jesusHour = 0;
-$joseHour = 0;
-$davidHour = 0;
 $currentUserHour = 0;
 $currentLowHour = 999;
-$winner = 0;
-$selectHour = "SELECT id, name, password, team, assignedHour FROM employee";
+
+$selectHour = "SELECT name, assignedHour FROM employee";
 $resultHour = $link->query($selectHour);
 
+$scheduleList = array();
+
 if ($resultHour->num_rows > 0) {
-while ($row = $resultHour->fetch_assoc()) {
-$userEmployee[$i3] = $row["name"];
-$userHour[$i3] = $row["assignedHour"];
-$i3++;
-}
-}
-
-function canCover($intshiftIndex, $availabilityIndex) {
-if ($month2[$availabilityIndex] == $month[$shiftIndex]){} else return FALSE;
-if ($day2[$availabilityIndex] == $day[$shiftIndex]){} else return FALSE;
-if ($hour2[$availabilityIndex] <= $hour[$shiftIndex]){} else return FALSE;
-if ($min2[$availabilityIndex] <= $min[$shiftIndex]){} else return FALSE;
-if ($ehour2[$availabilityIndex] >= $ehour[$shiftIndex]){} else return FALSE;
-if ($emin2[$availabilityIndex] >= $emin[$shiftIndex]){} else return FALSE;
-
-return TRUE;
+	while ($row = $resultHour->fetch_assoc()) {
+		$userEmployee[$i3] = $row["name"];
+		$userHour[$i3] = $row["assignedHour"];
+		$myEmployee[$i3] = new employee($userEmployee[$i3], $userHour[$i3]);
+		$i3++;
+	}
 }
 
-for($sCount = 0; $sCount < $i; $sCount++)
-{
-if($covered[$sCount] == 0)
-{
-for ($aCount = 0; $aCount < $i2; $aCount++) {
-$nameTime = $user2[$aCount];
-for ($u = 0; $u < $i3; $u++)
-{
-if($nameTime == "Jesus Salcedo")
-{
-if($jesusHour == 0)
-{
-$jesusHour = $userHour[$u];
-}
-$currentUserHour = $jesusHour;
-}
-if($nameTime == "Jose Gonzalez")
-{
-if($joseHour == 0)
-{
-$joseHour = $userHour[$u];
-}
-$currentUserHour = $joseHour;
-}
-if($nameTime == "David Cruz")
-{
-if($davidHour == 0)
-{
-$davidHour = $userHour[$u];
-}
-$currentUserHour = $davidHour;
-}
+function canCover($tempMyShift2, $tempEmployeeShift2) {
+	if ($tempEmployeeShift2->month == $tempMyShift2->month) {
+	} else {
+		return FALSE;
+	}
+
+	if ($tempEmployeeShift2->day == $tempMyShift2->day) {} else {
+		return FALSE;
+	}
+
+	if ($tempEmployeeShift2->hour <= $tempMyShift2->hour) {} else {
+		return FALSE;
+	}
+
+	if ($tempEmployeeShift2->min <= $tempMyShift2->min) {} else {
+		return FALSE;
+	}
+
+	if ($tempEmployeeShift2->endHour >= $tempMyShift2->endHour) {} else {
+		return FALSE;
+	}
+
+	if ($tempEmployeeShift2->endMin >= $tempMyShift2->endMin) {} else {
+		return FALSE;
+	}
+
+	return TRUE;
 }
 
-if (canCover($i, $i2) && $currentUserHour < $currentLowHour) {
-$user[$sCount] = $user2[$aCount];
-$currentLowHour = $currentUserHour;
-}
-}
-for ($u = 0; $u < $i3; $u++)
-{
-if($user[$sCount] == "Jesus Salcedo")
-{
-$jesusHour = $jesusHour + 1;
-$winner = $jesusHour;
-break;
-}
-if($user[$sCount] == "Jose Gonzalez")
-{
-$joseHour = $joseHour + 1;
-$winner = $joseHour;
-break;
-}
-if($user[$sCount] == "David Cruz")
-{
-$davidHour = $davidHour + 1;
-$winner = $davidHour;
-break;
-}
-}
-}
+$employeeTaken = false;
 
-$currentLowHour = $winner;
+for ($shiftCount = 0; $shiftCount < $i; $shiftCount++) {
+	if ($covered[$shiftCount] == 0) {
+		for ($availCount = 0; $availCount < $i2; $availCount++) {
+			$tempMyShift2 = $myShift[$shiftCount];
+			$tempEmployeeShift2 = $myEmployeeSchedule[$availCount];
+			//Link the employee with his schedule
+			foreach ($myEmployee as $value) {
+				if ($value->name == $tempEmployeeShift2->name) {
+					$tempEmployee = $value;
+				}
+			}
+			if (canCover($tempMyShift2, $tempEmployeeShift2) && $tempEmployee->hoursAssigned < $currentLowHour) {
+				$tempMyShift = $myShift[$shiftCount];
+				$tempEmployeeShift = $myEmployeeSchedule[$availCount];
+
+				$tempMyShift = $tempEmployeeShift;
+
+				$user[$shiftCount] = $user2[$availCount];
+
+				$scheduleList[$shiftCount] = $tempMyShift;
+
+				$currentLowHour = $tempEmployee->hoursAssigned;
+				$employeeTaken = true;
+			} elseif (!$employeeTaken) {
+				$tempMyShift = $myShift[$shiftCount];
+				$tempMyShift->name = "Employee Not Available";
+				$scheduleList[$shiftCount] = $tempMyShift;
+
+			}
+		}
+		$employeeTaken = false;
+	}
+	$currentLowHour = $currentUserHour + 1;
+	//Update Information
+	foreach ($myEmployee as $value) {
+		if ($value->name == $tempMyShift->name) {
+			$value->hoursAssigned = $currentLowHour;
+		}
+	}
+
 }
 
 echo "<table>";
@@ -270,60 +245,68 @@ echo "<th>Start Time</th>";
 echo "<th>End Time</th>";
 echo "</tr>";
 
-//Iterates through all the schedules the emplyoee submitted and makes a table for them
+//Iterates through all the schedules the manager submitted and makes a table for them
 for ($x = 0; $x < $i; $x++) {
-echo "<tr>";
-//Makes a cloumn for their name
-echo "<td>" . $name . "</td>";
-//Makes a column for the date they are working
-echo "<td>" . $month[$x] . "/" . $day[$x] . "</td>";
+	$tempScheduleList = $scheduleList[$x];
+	echo "<tr>";
+//Makes a cloumn for their name and date they are working
+	echo "<td>" . $tempScheduleList->name . "</td>";
+	echo "<td>" . $tempScheduleList->month . "/" . $tempScheduleList->day . "</td>";
 //Makes a column for the time they are working
-//Checks to see if the time is before 10 mins because if it is then it adds a 0 before the minute
-if ($min[$x] < 10) {
+	//Checks to see if the time is before 10 mins because if it is then it adds a 0 before the minute
+	if ($tempScheduleList->min < 10) {
 //Checks to see whether the time is am or pm and formats the time into 12 hour loop
-if ($hour[$x] < 12) {
-$temp = $hour[$x] % 12;
-echo "<td>" . $temp . ":0" . $min[$x] . " am</td>";
-} else {
-$temp = $hour[$x] % 12;
-echo "<td>" . $temp . ":0" . $min[$x] . " pm</td>";
-}
-} else {
-//Checks to see whether the time is am or pm and formats the time into 12 hour loop
-if ($hour[$x] < 12) {
-$temp = $hour[$x] % 12;
-echo "<td>" . $temp . ":" . $hour[$x] . " am</td>";
-} else {
-$temp = $hour[$x] % 12;
-echo "<td>" . $temp . ":" . $hour[$x] . " pm</td>";
-}
-}
+		if ($tempScheduleList->hour < 12) {
+			echo "<td>" . $tempScheduleList->hour . ":0" . $tempScheduleList->min . " am</td>";
+		} else {
+			$temp = $tempScheduleList->hour % 12;
+			if ($temp == 0) {
+				$temp = 12;
+			}
+			echo "<td>" . $temp . ":0" . $tempScheduleList->min . " pm</td>";
+		}
+	} else {
+		if ($tempScheduleList->hour < 12) {
+			echo "<td>" . $tempScheduleList->hour . ":" . $tempScheduleList->min . " am</td>";
+		} else {
+			$temp = $tempScheduleList->hour % 12;
+			if ($temp == 0) {
+				$temp = 12;
+			}
+
+			echo "<td>" . $temp . ":" . $tempScheduleList->min . " pm</td>";
+		}
+	}
 //Makes a column for the end time
-//Checks to see if the end time is before 10 mins because if it is then it adds a 0 before the minute
-if ($emin[$x] < 10) {
+	//Checks to see if the end time is before 10 mins because if it is then it adds a 0 before the minute
+	if ($tempScheduleList->endMin < 10) {
 //Checks to see whether the time is am or pm and formats the time into 12 hour loop
-if ($ehour[$x] < 12) {
-$temp = $ehour[$x] % 12;
-echo "<td>" . $temp . ":0" . $emin[$x] . " am</td>";
-} else {
-$temp = $ehour[$x] % 12;
-echo "<td>" . $temp . ":0" . $emin[$x] . " pm</td>";
-}
-} else {
-//Checks to see whether the time is am or pm and formats the time into 12 hour loop
-if ($ehour[$x] < 12) {
-$temp = $ehour[$x] % 12;
-echo "<td>" . $temp . ":" . $emin[$x] . " am</td>";
-} else {
-$temp = $ehour[$x] % 12;
-echo "<td>" . $temp . ":" . $emin[$x] . " pm</td>";
-}
-}
-echo "</tr>";
+		if ($tempScheduleList->endHour < 12) {
+			echo "<td>" . $tempScheduleList->endHour . ":0" . $tempScheduleList->endMin . " am</td>";
+		} else {
+			$temp = $tempScheduleList->endHour % 12;
+			if ($temp == 0) {
+				$temp = 12;
+			}
+
+			echo "<td>" . $temp . ":0" . $tempScheduleList->endMin . " pm</td>";
+		}
+	} else {
+		if ($tempScheduleList->endHour < 12) {
+			echo "<td>" . $tempScheduleList->endHour . ":" . $tempScheduleList->endMin . " am</td>";
+		} else {
+			$temp = $tempScheduleList->endHour % 12;
+			if ($temp == 0) {
+				$temp = 12;
+			}
+
+			echo "<td>" . $temp . ":" . $tempScheduleList->endMin . " pm</td>";
+		}
+	}
+	echo "</tr>";
 }
 
 echo "</table>";
- */
 
 mysqli_close($link);
 
